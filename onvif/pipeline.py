57662d390899da0c0cde09f119c596b443d21414
nunu <shaoqiang.chen@intel.com>
2023-07-27 18:58:22 +0800

    url and model are configurable

--- onvif/pipeline.py
+++ onvif/pipeline.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import os
-import sys
-import logging
 from flask import Flask, request
+
+workDir = "/home/dlstreamer/"
+videoDir = workDir + "videos/"
+modelDir = workDir + "models/"
 
 app = Flask(__name__)
 
@@ -13,8 +15,9 @@ def hello():
 
 @app.route('/pipeline', methods=['POST'])
 def pipeline():
-    url = ""
+    url = "dayu.mp4"
     dev = "CPU"
+    model = "horizontal-text-detection-0001.xml"
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         json = request.get_json()
@@ -24,13 +27,18 @@ def pipeline():
             return "url must not be null"
         if "dev" in json:
             dev = json["dev"]
+        if "model" in json:
+            model = json["model"]
+        if not url.startswith("http://"):
+            url = videoDir + url
+        model = modelDir + model
         pipeline_cmd = f'gst-launch-1.0 -v filesrc location={url} ! decodebin ! videoconvert n-threads=4 ! \
-capsfilter caps="video/x-raw,format=BGRx" ! gvainference model-instance-id=nunu model="model" device={dev} ! \
+capsfilter caps="video/x-raw,format=BGRx" ! gvainference model-instance-id=nunu model={model} device={dev} ! \
 queue ! gvafpscounter ! fakesink async=false'
         os.system(pipeline_cmd)
         return pipeline_cmd
     else:
         return 'Content-Type not supported!'
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=55555)
