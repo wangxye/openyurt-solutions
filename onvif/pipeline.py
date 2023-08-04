33bf2a1d16310b29f7fe37dc9a4c1d87370ce3bd
nunu <shaoqiang.chen@intel.com>
2023-08-04 15:28:27 +0800

    add src
    Signed-off-by: nunu <shaoqiang.chen@intel.com>

--- onvif/pipeline.py
+++ onvif/pipeline.py
@@ -3,6 +3,11 @@
 import os
 from flask import Flask, request
 
+'''
+    pipeline server run with dlstreamer
+    port: 55555
+'''
+
 workDir = "/home/dlstreamer/"
 videoDir = workDir + "videos/"
 modelDir = workDir + "models/"
@@ -15,9 +20,11 @@ def hello():
 
 @app.route('/pipeline', methods=['POST'])
 def pipeline():
-    url = "dayu.mp4"
+    src = "rtspsrc"
+    url = ""
     dev = "CPU"
     model = "horizontal-text-detection-0001.xml"
+
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         json = request.get_json()
@@ -25,20 +32,28 @@ def pipeline():
             url = json["url"]
         else:
             return "url must not be null"
+        if "src" in json:
+            src = json["src"]
         if "dev" in json:
             dev = json["dev"]
         if "model" in json:
             model = json["model"]
-        if not url.startswith("http://"):
-            url = videoDir + url
-        model = modelDir + model
-        pipeline_cmd = f'gst-launch-1.0 -v filesrc location={url} ! decodebin ! videoconvert n-threads=4 ! \
-capsfilter caps="video/x-raw,format=BGRx" ! gvainference model-instance-id=nunu model={model} device={dev} ! \
-queue ! gvafpscounter ! fakesink async=false'
-        os.system(pipeline_cmd)
-        return pipeline_cmd
+
+        pipeline_cmd = create_pipeline(src=src, url=url, model=model, dev=dev)
+        return pipeline_cmd + " created"
     else:
         return 'Content-Type not supported!'
+
+def create_pipeline(src, url, model, dev):
+    model = modelDir + model
+    if src == "filesrc":
+        url = videoDir + url
+
+    pipeline_cmd = f'gst-launch-1.0 -v {src} location={url} ! decodebin ! videoconvert n-threads=4 ! \
+capsfilter caps="video/x-raw,format=BGRx" ! gvainference model-instance-id=nunu model={model} device={dev} ! \
+queue ! gvafpscounter ! fakesink async=false'
+    #os.system(pipeline_cmd)
+    return pipeline_cmd
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=55555)
